fe6a3b0fc2a6c24797dc1fa7fc089e6bca46d3dc
yogita pandurang chaudhari <80738479+yogitach01@users.noreply.github.com>
2022-04-21 15:51:38 +0530

    added heartdisease route

--- server/route/route.js
+++ server/route/route.js
@@ -1,6 +1,6 @@
 import express from  'express';
-//import {heartdisease} from '../disease/heartdisease.js';
+import {heartdisease} from '../disease/heartdisease.js';
 const router = express.Router();
 
-//router.post('/heart',heartdisease);
+router.post('/heart',heartdisease);
 export default router;
