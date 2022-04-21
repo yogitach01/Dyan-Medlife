6250db41d699735dc15cf433955be041f6e0f189
AniketChauhan193 <80771166+aniket193-web@users.noreply.github.com>
2022-04-21 13:57:49 +0530

    added index.js and route for backend

---
+++ route/route.js
@@ -0,0 +1,6 @@
+import express from  'express';
+//import {heartdisease} from '../disease/heartdisease.js';
+const router = express.Router();
+
+//router.post('/heart',heartdisease);
+export default router;
