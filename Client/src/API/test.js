13ba9e03fa9d49c65d13584406ce5e7385db03b5
AniketChauhan193 <80771166+aniket193-web@users.noreply.github.com>
2022-04-22 01:46:12 +0530

    Add files via upload

---
+++ Client/src/API/test.js
@@ -0,0 +1,55 @@
+const test = [{
+    id: 1,
+    value: "Jaundice",
+    tests: "Bilirubin blood test "
+},
+{
+    id: 2,
+    value: "Malaria",
+    tests: "Rapid Diagonstic Test"
+},
+
+{
+    id: 3,
+    value: "Dengue",
+    tests: "NS1 Antigen,RT-PCR test"
+},
+{
+    id: 4,
+    value: "Typhoid",
+    tests: "Widal test"
+},
+{
+    id: 5,
+    value: "Hepatitis A",
+    tests: "IgM(immunoglobin M),IgG(immunoglobulin G)"
+
+},
+{
+    id: 6,
+    value: "Hepatitis B",
+    tests: "HBV",
+},
+{
+    id: 7,
+    value: "Hepatitis C",
+    tests: "HCV"
+},
+{
+    id: 8,
+    value: "Hepatitis D",
+    tests: "HDV"
+},
+
+{
+    id: 9,
+    value: "Pneumonia",
+    tests: "Chest x-ray"
+
+},
+
+
+
+
+]
+
