4d4c36f042e83a3dc32b52e37cb377ab2ac57edb
AniketChauhan193 <80771166+aniket193-web@users.noreply.github.com>
2022-04-21 13:58:09 +0530

    Delete route.js

--- route/route.js
+++
@@ -1,6 +0,0 @@
-import express from  'express';
-//import {heartdisease} from '../disease/heartdisease.js';
-const router = express.Router();
-
-//router.post('/heart',heartdisease);
-export default router;
