15bf812b8f03de8480e54add8cd3453281519174
yogita pandurang chaudhari <80738479+yogitach01@users.noreply.github.com>
2022-04-22 01:19:08 +0530

    added api for disease

--- Client/src/API/workapi.js
+++ Client/src/API/workapi.js
@@ -1,25 +1,42 @@
 const workapi = [
-    {
-      id: 1,
-      logo: "fa fa-heartbeat",
-      title: "Predict Your Heart Disease",
-      info: "Intelligent heart disease prediction system using data mining techniques. Using medical profiles such as age, sex, blood pressure and blood sugar it can predict the likelihood of patients getting a heart disease. It enables significant knowledge, e.g. patterns, relationships between medical factors related to heart disease, to be established.",
-      link:"/pre"
-    },
-    {
-      id: 2,
-      logo: "fas fa-chalkboard-teacher",
-      title: "Predict Your Breast Cancer ",
-      info: " With a DYAN MEDLIFE Doctor Profile, you can connect to potential patients in no time. It helps you Connect with patients directly, Maintain digital presence, Respond to patients and their queries",
-      link:"/Btc"
-    },
-    {
-      id: 3,
-      logo: "fas fa-lungs",
-      title: "Predict Your Lung Cancer",
-      info: "Visit our website for emergency blood supply and ambulance supply . We connect blood donor with blood seeker so that a donor can see visible impact created by their blood in someone's life",
-      link:"/Lc"
-    },
-  ];
-  
-  export default workapi;
+  {
+    id: 1,
+    logo: "fa fa-user-md",
+    title: "Register for online consultation",
+    info:"Get on-boarded in 2 minutes and start your video consultation with the patients instantly.",
+  link:"/consultant"
+  },
+  {
+    id: 2,
+    logo: "fa fa-heartbeat",
+    title: "Predict Heart Diseases ",
+    info: "Intelligent heart disease prediction system using data mining techniques. Using medical profiles such as age, sex, blood pressure and blood sugar it can predict the likelihood of patients getting a heart disease. It enables significant knowledge, e.g. patterns, relationships between medical factors related to heart disease, to be established.",
+    link:"/pre"
+  },
+
+  {
+    id: 3,
+    logo: "fa fa-heartbeat",
+    title: "Predict breast Cancer ",
+    info: "Breast cancer prediction is a diagnosis tool. Oncologists and medical staff face the challenge of identifying breast cancer as soon as possible. An early diagnosis increases survival chances. It can help them diagnose malignant tumours quicker.",
+    link:"/Btc"
+  },
+  {
+    id: 4,
+    logo: "fa fa-heartbeat",
+    title: "Lung Cancer Prediction",
+    info:"The prominent cause of cancer-related mortality throughout the globe is “Lung Cancer”. Hence beforehand detection, prediction and diagnosis of lung cancer has become essential as it expedites and simplifies the consequent clinical board. To erect the progress and medication of cancerous conditions machine learning techniques have been utilized because of its accurate outcomes",
+    link:"/lungcancer"
+  },
+  {
+    id:5,
+    logo: "fa fa-user-md",
+    title:"Diabetes prediction",
+    info:"Diabetes is a chronic disease that occurs either when the pancreas does not produce enough insulin or when the body cannot effectively use the insulin it produces. Insulin is a hormone that regulates blood sugar.",
+    link:"/Diabetes"
+  }
+
+ 
+];
+
+export default workapi;
